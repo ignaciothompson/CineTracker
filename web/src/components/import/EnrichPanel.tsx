f833c6c4ef1ef75567ce5d22c18c7f0f8131fffc
Ignacio Thompson <chonathomp@gmail.com>
2026-07-05 22:08:08 -0300

    ajustes deploy

--- web/src/components/import/EnrichPanel.tsx
+++ web/src/components/import/EnrichPanel.tsx
@@ -1,5 +1,5 @@
 import { useMemo, useState } from 'react';
-import { useLibraryContext, useTmdbContext } from '../context/AppContext';
+import { useLibraryContext, useTmdbContext } from '../../context/AppContext';
 import {
   countPendingEnrich,
   enrichLibrary,
